7de7232875baf01f9bd5f5b9bf34cbbecc1bd80f
Ruther Dio <111130814+rthrx44@users.noreply.github.com>
2023-07-05 03:59:00 +0800

    create landing page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,61 @@
 import React from 'react'
 import './Navbar.css';
+import { RdButton, RlButton } from '../buttons/Button';
+import { FaBars } from "react-icons/fa";
+import Cvlogo from '../assets/Cvlogo.png';
 
-export const Navbar = () => {
+export default function Navbar () {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href='#home'> Home </a>
-        </li>
-        <li>
-          <a href='#about'> About Us </a>
-        </li>
-        <li>
-          <a href='#contact'> Contact Us </a>
-        </li>
-      </ul>
-    </nav>
+    <header className='header container'>
+      <nav className='header__wrapper'>
+        <img id='#home' className='header__logo' src={Cvlogo} alt='Covape-19 Garage Logo' />
+        <ul className='header__menu'>
+          <li>
+            <a className='header__link' href='#home'> Home </a>
+          </li>
+          <li>
+            <a className='header__link' href='#about'> About Us </a>
+          </li>
+          <li>
+            <a className='header__link' href='#contact'> Contact Us </a>
+          </li>
+        </ul>
+        <ul className='header__btns'>
+          <li className='header__login'> <RlButton displayText = 'Login'/></li>
+          <li className='header__login'> <RdButton displayText = 'Sign Up'/> </li>
+        </ul>
+        <button className='header__bars'><FaBars/></button>
+      </nav>
+    </header>
   )
 }
 
 
 export const DashNavbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='header container'>
+      <nav className='header__wrapper'>
+        <img className='header__logo' src={Cvlogo} alt='Covape-19 Garage Logo' />
+        <ul className='header__menu'>
+          <li>
+            <a className='header__link' href='#device'> Vape Device </a>
+          </li>
+          <li>
+            <a className='header__link' href='#juice'> E-Juice </a>
+          </li>
+          <li>
+            <a className='header__link' href='#pod'> Disposable Pod </a>
+          </li>
+          <li>
+            <a className='header__link' href='#accessories'> Accessories </a>
+          </li>
+        </ul>
+        <ul className='header__btns'>
+          <li className='header__login'> <RdButton displayText = 'Login'/></li>
+          <li className='header__login'> <RdButton displayText = 'Logout'/> </li>
+        </ul>
+        <button className='header__bars'><FaBars/></button>
+      </nav>
+    </header>
   )
 }
