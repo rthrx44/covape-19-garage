e0f80370b612ce0d14637c8a63c74f862a1eddf3
Ruther Dio <111130814+rthrx44@users.noreply.github.com>
2023-07-06 19:28:24 +0800

    fixing mobile nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './styles/Modern-normalize.css';
 import './styles/App.css';
 import './styles/Utils.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/landing/Landing';
+
 
 function App() {
   return (
     <>
-    <Landing/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Landing/>}/>
+        <Route path='/about' element={<Landing/>}/>
+        <Route path='/contact' element={<Landing/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,60 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
 import { RButton } from '../buttons/Button';
 import { FaBars } from "react-icons/fa";
-import Cvlogo from '../assets/Cvlogo.png';
+import Cvlogo from '../assets/Cvlogo.png'; 
 
 export default function Navbar () {
-  return (
-    <header id='home' className='header container'>
-      <nav className='header__wrapper'>
-        <img className='header__logo' src={Cvlogo} alt='Covape-19 Garage Logo' />
-        <ul className='header__menu'>
-          <li>
-            <a className='header__link' href='#home'> Home </a>
-          </li>
-          <li>
-            <a className='header__link' href='#about'> About Us </a>
-          </li>
-          <li>
-            <a className='header__link' href='#contact'> Contact Us </a>
-          </li>
-        </ul>
-        <ul className='header__btns'>
-          <li className='header__login'> <RButton displayText = 'Login'/></li>
-        </ul>
-        <button className='header__bars'><FaBars/></button>
-      </nav>
-    </header>
-  )
-}
 
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export const DashNavbar = () => {
+  const handletoggle = () => {
+    const body = document.querySelector('body')
+    const mobileLinks = document.querySelectorAll('.mobile__link');
+
+    if (toggleMenu) {
+      setToggleMenu(false)
+      body.style.overflowY = 'auto'
+    } else {
+      setToggleMenu(true)
+      body.style.overflowY = 'hidden'
+    };
+
+    mobileLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        setToggleMenu(false)
+        document.body.style.overflowY = 'hidden';
+      });
+      console.log(mobileLinks);
+    });
+  }
+
   return (
-    <header className='header container'>
-      <nav className='header__wrapper'>
-        <img className='header__logo' src={Cvlogo} alt='Covape-19 Garage Logo' />
-        <ul className='header__menu'>
-          <li>
-            <a className='header__link' href='#device'> Vape Device </a>
-          </li>
-          <li>
-            <a className='header__link' href='#juice'> E-Juice </a>
-          </li>
-          <li>
-            <a className='header__link' href='#pod'> Disposable Pod </a>
-          </li>
-          <li>
-            <a className='header__link' href='#accessories'> Accessories </a>
-          </li>
-        </ul>
-        <ul className='header__btns'>
-          <li className='header__login'> <RButton displayText = 'Login'/></li>
-          <li className='header__login'> <RButton displayText = 'Logout'/> </li>
-        </ul>
-        <button className='header__bars'><FaBars/></button>
-      </nav>
-    </header>
+    <>
+      {toggleMenu && 
+        <nav className='mobile__wrapper'>
+          <ul className='mobile__menu'>
+            <li>
+              <a className='mobile__link' href='#home'> Home </a>
+            </li>
+            <li>
+              <a className='mobile__link' href='#about'> About Us </a>
+            </li>
+            <li>
+              <a className='mobile__link' href='#contact'> Contact Us </a>
+            </li>
+            <li className='mobile__link-line'></li>
+            <li className='mobile__login'> <RButton displayText = 'Login'/></li>
+          </ul>
+        </nav>
+        }
+      <header id='home' className='header container'>
+        <nav className='header__wrapper'>
+          <img className='header__logo' src={Cvlogo} alt='Covape-19 Garage Logo' />
+          <ul className='header__menu'>
+            <li>
+              <a className='header__link' href='#home'> Home </a>
+            </li>
+            <li>
+              <a className='header__link' href='#about'> About Us </a>
+            </li>
+            <li>
+              <a className='header__link' href='#contact'> Contact Us </a>
+            </li>
+          </ul>
+          <ul className='header__btns'>
+            <li className='header__login'> <RButton displayText = 'Login'/></li>
+          </ul>
+          <button className='header__bars' onClick={handletoggle}><FaBars/></button>
+        </nav>
+      </header>
+    </>
   )
 }
