38887dd0095d3c5110d851535aefb44f2465a2cd
Ruther Dio <111130814+rthrx44@users.noreply.github.com>
2023-07-07 17:08:43 +0800

    change landing page to dashboard tweak css on navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './styles/Modern-normalize.css';
 import './styles/App.css';
 import './styles/Utils.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/landing/Landing';
+import Dashboard from './pages/dashboard/Dashboard';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
 
@@ -12,7 +12,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
+        <Route path='/' element={<Dashboard/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
       </Routes>
